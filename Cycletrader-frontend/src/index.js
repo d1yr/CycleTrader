ec3cfa091dd4b8ea75822790d23aa33f0f4dfcc1
D1yr <alexander.m.sahli@gmail.com>
2020-10-23 09:47:02 -0400

    RD done, working on CU

--- Cycletrader-frontend/src/index.js
+++ Cycletrader-frontend/src/index.js
@@ -66,23 +66,21 @@ const clickHandler = () => {
             .then(renderBicycles)
         }
         const renderBicycles = bicycles => {
+            const bikeUl = document.querySelector('#bikes')
+            bikeUl.innerHTML += `<button class="addBike">Add Bicycle</button>`
             for(const bicycle of bicycles){
             renderBicycle(bicycle)
             if(bicycle.user_id == e.target.dataset.userId){
+
                 const bikeSpan = document.querySelector('#bikes')
                 const bikeUl = document.createElement('ul')
                 bikeUl.textContent += `BRAND: ${bicycle.brand}`
                 bikeUl.innerHTML += "<br>"
                 bikeUl.innerHTML += `MODEL: ${bicycle.model}`
                 bikeUl.innerHTML += "<br>"
-                bikeUl.innerHTML += `GEARSET: ${bicycle.gearset}`
-                bikeUl.innerHTML += "<br>"
-                bikeUl.innerHTML += `FRONT CHAINRINGS: ${bicycle.frontG}`
-                bikeUl.innerHTML += "<br>"
-                bikeUl.innerHTML += `REAR COGS: ${bicycle.rearG}`
-                bikeUl.innerHTML += "<br>"
-                bikeUl.innerHTML += `CONDITION: ${bicycle.condition}`
-                console.log(bicycle.user_id)
+                bikeUl.innerHTML += `<button class="mInfo">More Info</button>`
+                bikeUl.dataset.userId = bicycle.user_id
+                bikeUl.dataset.bikeId = bicycle.id
                 
 
                 bikeSpan.append(bikeUl)
@@ -102,20 +100,102 @@ const clickHandler = () => {
             }
         }
         document.addEventListener('click', e => {
-            if(e.target.matches('ul')){
-            const info = document.querySelector('#detailed-info')
-            info.innerHTML = ''
-            console.log(e.target)
+            if(e.target.matches('.mInfo')){
 
-
+                
+              bikeId = e.target.parentElement.dataset.bikeId
+              fetch(baseUrlBikes + bikeId)
+            .then(resp => resp.json())
+            .then(bicycle => renderSpotlight(bicycle))
+            const renderSpotlight = bicycle => {
+                const info = document.querySelector('#bikes')
+                info.innerHTML = `BRAND: ${bicycle.brand}`
+                info.innerHTML += `<br>`
+                info.innerHTML += `MODEL: ${bicycle.model}`
+                info.innerHTML += `<br>`
+                info.innerHTML += `SIZE: ${bicycle.size}cm`
+                info.innerHTML += "<br>"
+                info.innerHTML += `GEARSET: ${bicycle.gearset}`
+                info.innerHTML += "<br>"
+                info.innerHTML += `FRONT CHAINRINGS: ${bicycle.frontG}`
+                info.innerHTML += "<br>"
+                info.innerHTML += `REAR COGS: ${bicycle.rearG}`
+                info.innerHTML += "<br>"
+                info.innerHTML += `CONDITION: ${bicycle.condition}`
+                info.innerHTML += "<br>"
+                info.innerHTML += `<button class="deleteBike">Delete Bicycle</button>`
+                info.innerHTML += "<br>"
+                info.innerHTML += `<button class="editBike">Edit Bicycle</button>`
+                info.dataset.userId = bicycle.user_id
+                info.dataset.bikeId = bicycle.id
 
             }
+        }
+        document.addEventListener('click', e => {
+            if(e.target.matches('.deleteBike')){
+            bikeId = e.target.parentElement.dataset.bikeId
+            deleteData(bikeId, baseUrlBikes)
+            
+
+
+        }})
+        document.addEventListener('click', e => {
+            if(e.target.matches('.addBike')){
+
+                console.log(e.target)
+            const info = document.querySelector('#bikes')
+            info.innerHTML += `<form id='edit-bike-form' class="padding margin border-round border-grey">
+            <input type="hidden" value= "bikeId" id= "bikeId"/>Brand:
+            <input type="text" name="brand" placeholder="Bike Brand" value="" />
+            Model:
+            <input type="text" name="model" placeholder="Bike Model" value="" />
+            Size:<input type="number" name="size" min='1' max='70' value="" />
+            <br>
+            <label for="gearset">Gearset:</label>
+            <select id="gearset" name="gearset">
+            <option value="Campagnolo">Campagnolo</option>
+            <option value="Shimano">Shimano</option>
+            <option value="SRAM">SRAM</option>
+            </select>
+            Front Chainring:<input type="number" name="frontG" min='1' max='3' value="" />
+            Rear Cog:<input type="number" name="rearG" min='1' max='11' value="" />
+            <br>
+            <label for="condition">Condition:</label>
+            <select id="condition" name="condition">
+            <option value="new">New</option>
+            <option value="excellent">Excellent</option>
+            <option value="good">Good</option>
+            <option value="poor">Poor</option>
+            <option value-"parts">Parts</option>
+            </select>
+            <input type="submit" value="Submit" />
+
+
+          </form>`
+          
+            
+                
+
+            
+            
+
+
+        }})
         })
+        
         getBikes ()
     })
     
 }
-
+function deleteData(item, url) {
+    return fetch(url + '/' + item, {
+      method: 'delete'
+    }).then(response =>
+      response.json().then(json => {
+        return json;
+      })
+    );
+  }
 
 
 
